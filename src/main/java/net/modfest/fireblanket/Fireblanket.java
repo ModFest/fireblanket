d7824ca0cc86f9846ff40b6884ce03bbe4e7ada6
Jasmine Karthikeyan <25208576+jaskarth@users.noreply.github.com>
2023-08-12 17:05:18 -0400

    Dump entities command; Add system to force configuration of entity types

--- src/main/java/net/modfest/fireblanket/Fireblanket.java
+++ src/main/java/net/modfest/fireblanket/Fireblanket.java
@@ -2,10 +2,16 @@ package net.modfest.fireblanket;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 import net.modfest.fireblanket.command.DumpCommandBlocksCommand;
+import net.modfest.fireblanket.command.DumpEntityTypesCommand;
+import net.modfest.fireblanket.world.entity.EntityFilters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Fireblanket implements ModInitializer {
 	public static final Identifier BATCHED_BE_UPDATE = new Identifier("fireblanket", "batched_be_sync");
@@ -14,5 +20,16 @@ public class Fireblanket implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		DumpCommandBlocksCommand.init();
+
+		DumpEntityTypesCommand.init();
+
+		Path configs = FabricLoader.getInstance().getConfigDir().resolve("fireblanket");
+		if (Files.exists(configs)) {
+			Path types = configs.resolve("entityfilters.txt");
+
+			if (Files.exists(types)) {
+				EntityFilters.parse(types);
+			}
+		}
 	}
 }
---
+++ src/main/java/net/modfest/fireblanket/mixin/entity_ticking/MixinThreadedAnvilChunkStorage.java
@@ -0,0 +1,20 @@
+package net.modfest.fireblanket.mixin.entity_ticking;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.server.world.ThreadedAnvilChunkStorage;
+import net.modfest.fireblanket.world.entity.EntityFilters;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ThreadedAnvilChunkStorage.class)
+public class MixinThreadedAnvilChunkStorage {
+    @Redirect(method = "loadEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityType;alwaysUpdateVelocity()Z"))
+    private boolean fireblanket$forceVelocityOff(EntityType<?> instance) {
+        if (EntityFilters.isTypeForcedVelocityOff(instance)) {
+            return false;
+        }
+
+        return instance.alwaysUpdateVelocity();
+    }
+}
