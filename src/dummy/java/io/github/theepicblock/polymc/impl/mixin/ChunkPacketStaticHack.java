1e5f46f2a7daf6a910d36254a371528e898eb8a4
Una Thompson <me@unascribed.com>
2023-08-15 04:41:39 -0700

    PolyMc compatibility

---
+++ src/dummy/java/io/github/theepicblock/polymc/impl/mixin/ChunkPacketStaticHack.java
@@ -0,0 +1,7 @@
+package io.github.theepicblock.polymc.impl.mixin;
+
+import net.minecraft.server.network.ServerPlayerEntity;
+
+public class ChunkPacketStaticHack {
+    public static ThreadLocal<ServerPlayerEntity> player = new ThreadLocal<>();
+}
