3dbf121408a7a0f11768700530a21eb32bca2b55
Jasmine Karthikeyan <25208576+jaskarth@users.noreply.github.com>
2023-08-15 19:24:57 -0400

    Forcibly stuff all sign blocks into their respective BE types

--- src/main/java/net/modfest/fireblanket/Fireblanket.java
+++ src/main/java/net/modfest/fireblanket/Fireblanket.java
@@ -2,10 +2,15 @@ package net.modfest.fireblanket;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.block.Block;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 import net.modfest.fireblanket.command.DumpCommandBlocksCommand;
 import net.modfest.fireblanket.command.DumpEntityTypesCommand;
+import net.modfest.fireblanket.world.blocks.UpdateSignBlockEntityTypes;
 import net.modfest.fireblanket.world.entity.EntityFilters;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,6 +26,14 @@ public class Fireblanket implements ModInitializer {
 	public void onInitialize() {
 		DumpCommandBlocksCommand.init();
 		DumpEntityTypesCommand.init();
+
+		for (Block block : Registries.BLOCK) {
+			UpdateSignBlockEntityTypes.apply(block);
+		}
+
+		RegistryEntryAddedCallback.event(Registries.BLOCK).register((r, id, block) -> {
+			UpdateSignBlockEntityTypes.apply(block);
+		});
 
 		Path configs = FabricLoader.getInstance().getConfigDir().resolve("fireblanket");
 		if (Files.exists(configs)) {
---
+++ src/main/java/net/modfest/fireblanket/world/WorldLoadAppliers.java
@@ -0,0 +1,17 @@
+package net.modfest.fireblanket.world;
+
+import net.modfest.fireblanket.world.blocks.UpdateSignBlockEntityTypes;
+import net.modfest.fireblanket.world.entity.EntityFilters;
+
+public class WorldLoadAppliers {
+    private static boolean ran = false;
+
+    public static void init() {
+        if (ran) {
+            return;
+        }
+        ran = true;
+
+        EntityFilters.apply();
+    }
+}
