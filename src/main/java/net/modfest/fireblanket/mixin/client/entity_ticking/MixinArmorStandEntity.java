7a8a0f96768398537036c3fe5640305fd423624c
Jasmine Karthikeyan <25208576+jaskarth@users.noreply.github.com>
2023-08-17 20:48:36 -0400

    Disable side entity ticking for select entities

---
+++ src/main/java/net/modfest/fireblanket/mixin/client/entity_ticking/MixinArmorStandEntity.java
@@ -0,0 +1,27 @@
+package net.modfest.fireblanket.mixin.client.entity_ticking;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.decoration.ArmorStandEntity;
+import net.minecraft.world.World;
+import net.modfest.fireblanket.client.EntityTick;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ArmorStandEntity.class)
+public abstract class MixinArmorStandEntity extends LivingEntity {
+    protected MixinArmorStandEntity(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+    }
+
+    @Redirect(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;tick()V"))
+    private void fireblanket$noClientTick(LivingEntity instance) {
+        if (this.getWorld().isClient) {
+            EntityTick.minimalTick(instance);
+        } else {
+            instance.tick();
+        }
+    }
+}
