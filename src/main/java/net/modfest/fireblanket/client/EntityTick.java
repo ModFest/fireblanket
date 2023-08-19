555436b1145b874dbc96d960cd0e4e4794ca29cb
Jasmine Karthikeyan <25208576+jaskarth@users.noreply.github.com>
2023-08-18 23:49:17 -0400

    Fix not all entity properties being ticked

--- src/main/java/net/modfest/fireblanket/client/EntityTick.java
+++ src/main/java/net/modfest/fireblanket/client/EntityTick.java
@@ -6,6 +6,9 @@ public class EntityTick {
     public static void minimalTick(LivingEntity e) {
         e.prevPitch = e.getPitch();
         e.prevYaw = e.getYaw();
+        e.prevHeadYaw = e.headYaw;
+        e.prevBodyYaw = e.bodyYaw;
+
         // TODO: seems like only players use this?
 //        e.prevHorizontalSpeed = e.horizontalSpeed;
     }
