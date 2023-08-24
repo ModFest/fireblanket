53116726f5b0d45d0db468a146f1d5594d9c7d1e
Jasmine Karthikeyan <25208576+jaskarth@users.noreply.github.com>
2023-08-24 18:16:00 -0400

    Dump subcommand for loaded entities

--- src/main/java/net/modfest/fireblanket/command/DumpCommand.java
+++ src/main/java/net/modfest/fireblanket/command/DumpCommand.java
@@ -1,21 +1,29 @@
 package net.modfest.fireblanket.command;
 
+import it.unimi.dsi.fastutil.objects.Object2IntMap;
+import it.unimi.dsi.fastutil.objects.Object2IntOpenHashMap;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.CommandBlockBlockEntity;
 import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.registry.Registries;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.world.ChunkHolder;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.chunk.WorldChunk;
 
 import static net.minecraft.server.command.CommandManager.literal;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
@@ -73,6 +81,35 @@ public class DumpCommand {
 						server.getSource().sendFeedback(() -> Text.literal(Registries.ENTITY_TYPE.getId(type) + " alwaysUpdateVelocity=" + type.alwaysUpdateVelocity() + " updateDistance(blocks)=" + (type.getMaxTrackDistance() * 16) + " tickInterval=" + type.getTrackTickInterval()), false);
 					}
 	
+					return 0;
+				})
+			)
+			.then(literal("entities")
+				.executes(cmd -> {
+					MinecraftServer server = cmd.getSource().getServer();
+					server.submit(() -> {
+						for (ServerWorld world : server.getWorlds()) {
+							cmd.getSource().sendFeedback(() -> Text.literal("----- Dumping types for dimension " + world.getDimensionKey().getValue() + " --------"), false);
+
+							Object2IntOpenHashMap<EntityType<?>> map = new Object2IntOpenHashMap<>();
+
+							for (Entity entity : world.iterateEntities()) {
+								int r = map.getOrDefault(entity.getType(), 0);
+								map.put(entity.getType(), r + 1);
+							}
+
+							ArrayList<Object2IntMap.Entry<EntityType<?>>> entries = new ArrayList<>(map.object2IntEntrySet());
+							entries.sort(Map.Entry.comparingByValue());
+							Collections.reverse(entries);
+							for (Object2IntMap.Entry<EntityType<?>> e : entries) {
+								EntityType<?> key = e.getKey();
+								String string = Registries.ENTITY_TYPE.getId(key).toString();
+
+								cmd.getSource().sendFeedback(() -> Text.literal(string + " -> " + e.getIntValue()), false);
+							}
+						}
+					});
+
 					return 0;
 				})
 			)
