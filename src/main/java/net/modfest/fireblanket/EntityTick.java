829581e5497297a8e201493d45e3138701cbfd5c
Patbox <39821509+Patbox@users.noreply.github.com>
2023-08-24 23:48:08 +0200

    Fix Armor Stand and Core Physics movement on client (#17)
    * Fix Armor Stand and Core Physics movement on client
    
    * Make movement feel a bit smoother

--- src/main/java/net/modfest/fireblanket/EntityTick.java
+++ src/main/java/net/modfest/fireblanket/EntityTick.java
@@ -2,20 +2,39 @@ package net.modfest.fireblanket;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.MathHelper;
 
 public class EntityTick {
 	public static void minimalTick(Entity e) {
 		e.prevPitch = e.getPitch();
 		e.prevYaw = e.getYaw();
 		e.prevHorizontalSpeed = e.horizontalSpeed;
-		e.prevX = e.lastRenderX = e.getX();
-		e.prevY = e.lastRenderY = e.getY();
-		e.prevZ = e.lastRenderZ = e.getZ();
+		e.prevX = e.getX();
+		e.prevY = e.getY();
+		e.prevZ = e.getZ();
 	}
-	public static void minimalTick(LivingEntity e) {
-		minimalTick((Entity)e);
+
+	public static void minimalLivingTick(LivingEntity e) {
+		minimalTick(e);
 		e.prevHeadYaw = e.headYaw;
 		e.prevBodyYaw = e.bodyYaw;
 		e.lastHandSwingProgress = e.handSwingProgress;
+
+		if (e.bodyTrackingIncrements > 0) {
+			double x = e.getX() + (e.serverX - e.getX()) / e.bodyTrackingIncrements;
+			double y = e.getY() + (e.serverY - e.getY()) / e.bodyTrackingIncrements;
+			double z = e.getZ() + (e.serverZ - e.getZ()) / e.bodyTrackingIncrements;
+			double yaw = MathHelper.wrapDegrees(e.serverYaw - e.getYaw());
+			e.setYaw(e.getYaw() + (float)yaw / e.bodyTrackingIncrements);
+			e.setPitch((float) (e.getPitch() + (e.serverPitch - e.getPitch()) / e.bodyTrackingIncrements));
+			e.bodyYaw = e.getYaw();
+			--e.bodyTrackingIncrements;
+			e.setPosition(x, y, z);
+		}
+
+		if (e.headTrackingIncrements > 0) {
+			e.headYaw += (float)MathHelper.wrapDegrees(e.serverHeadYaw - (double)e.headYaw) / (float)e.headTrackingIncrements;
+			--e.headTrackingIncrements;
+		}
 	}
 }
