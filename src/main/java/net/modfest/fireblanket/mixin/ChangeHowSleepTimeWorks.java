0ace777f4056331ac4f96339f1a0ff6fbae16854
TheEpicBlock <git.teb@theepicblock.nl>
2024-02-09 23:21:52 +0100

    cerulean/moonrise interplay

---
+++ src/main/java/net/modfest/fireblanket/mixin/ChangeHowSleepTimeWorks.java
@@ -0,0 +1,31 @@
+package net.modfest.fireblanket.mixin;
+
+import net.minecraft.registry.DynamicRegistryManager;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.world.MutableWorldProperties;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import java.util.function.Supplier;
+
+@Mixin(ServerWorld.class)
+public abstract class ChangeHowSleepTimeWorks extends World {
+    protected ChangeHowSleepTimeWorks(MutableWorldProperties properties, RegistryKey<World> registryRef, DynamicRegistryManager registryManager, RegistryEntry<DimensionType> dimensionEntry, Supplier<Profiler> profiler, boolean isClient, boolean debugWorld, long biomeAccess, int maxChainedNeighborUpdates) {
+        super(properties, registryRef, registryManager, dimensionEntry, profiler, isClient, debugWorld, biomeAccess, maxChainedNeighborUpdates);
+    }
+
+    // Hacked in fix for cerulean/moonrise compat
+    @ModifyArg(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;setTimeOfDay(J)V"))
+    long changeSleep(long input) {
+        if (this.isDay()) {
+            return input - 24000L;
+        }
+        return input;
+    }
+}
