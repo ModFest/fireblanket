eeda67ae0da6c5de92917d3f02f2d4e68cdf9d23
Jasmine Karthikeyan <25208576+jaskarth@users.noreply.github.com>
2025-04-24 15:34:35 -0400

    Use weakreferences

--- src/main/java/net/modfest/fireblanket/mixin/client/render_verification/MixinBlockEntityRenderDispatcher.java
+++ src/main/java/net/modfest/fireblanket/mixin/client/render_verification/MixinBlockEntityRenderDispatcher.java
@@ -12,30 +12,32 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.lang.ref.WeakReference;
+
 @Mixin(BlockEntityRenderDispatcher.class)
 public class MixinBlockEntityRenderDispatcher {
 	@Unique
-	private volatile BlockEntity fireblanket$verify;
+	private volatile WeakReference<BlockEntity> fireblanket$verify;
 	@Unique
 	private volatile int fireblanket$depth;
 
 	@Inject(method = "render(Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V", at = @At("HEAD"))
 	private <T extends BlockEntity> void fireblanket$verifyStart(T blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
 		if (FireblanketClient.VERIFY_RENDER) {
-			fireblanket$verify = blockEntity;
+			fireblanket$verify = new WeakReference<>(blockEntity);
 			fireblanket$depth = ((MatrixStackAccessor)matrices).getStack().size();
 		}
 	}
 
 	@Inject(method = "render(Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V", at = @At("HEAD"))
 	private <T extends BlockEntity> void fireblanket$verifyEnd(T blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
-		if (FireblanketClient.VERIFY_RENDER && fireblanket$verify == blockEntity) {
+		if (FireblanketClient.VERIFY_RENDER && fireblanket$verify.get() == blockEntity) {
 			int size = ((MatrixStackAccessor) matrices).getStack().size();
 			if (fireblanket$depth != size) {
 				throw new IllegalStateException("Block entity " + blockEntity.getClass() + " had different matrix depths before and after rendering: should be " + fireblanket$verify + " but was " + size);
 			}
-
-			fireblanket$verify = null;
 		}
+
+		fireblanket$verify = new WeakReference<>(null);
 	}
 }
--- src/main/java/net/modfest/fireblanket/mixin/client/render_verification/MixinEntityRenderer.java
+++ src/main/java/net/modfest/fireblanket/mixin/client/render_verification/MixinEntityRenderer.java
@@ -17,30 +17,32 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.lang.ref.WeakReference;
+
 @Mixin(EntityRenderer.class)
 public class MixinEntityRenderer {
 	@Unique
-	private volatile Entity fireblanket$verify;
+	private WeakReference<Entity> fireblanket$verify;
 	@Unique
 	private volatile int fireblanket$depth;
 
 	@Inject(method = "render(Lnet/minecraft/entity/Entity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("HEAD"))
 	private <T extends Entity> void fireblanket$verifyStart(T entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
 		if (FireblanketClient.VERIFY_RENDER) {
-			fireblanket$verify = entity;
+			fireblanket$verify = new WeakReference<>(entity);
 			fireblanket$depth = ((MatrixStackAccessor)matrices).getStack().size();
 		}
 	}
 
 	@Inject(method = "render(Lnet/minecraft/entity/Entity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At("TAIL"))
 	private <T extends Entity> void fireblanket$verifyEnd(T entity, float yaw, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
-		if (FireblanketClient.VERIFY_RENDER && fireblanket$verify == entity) {
+		if (FireblanketClient.VERIFY_RENDER && fireblanket$verify.get() == entity) {
 			int size = ((MatrixStackAccessor) matrices).getStack().size();
 			if (fireblanket$depth != size) {
 				throw new IllegalStateException("Entity " + entity.getClass() + " had different matrix depths before and after rendering: should be " + fireblanket$verify + " but was " + size);
 			}
 		}
 
-		fireblanket$verify = null;
+		fireblanket$verify = new WeakReference<>(null);
 	}
 }
