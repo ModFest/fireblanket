fda53fcd6049c3a03bfb0eccfce17cb3ffabf01b
Jasmine Karthikeyan <25208576+jaskarth@users.noreply.github.com>
2025-04-24 21:18:26 -0400

    Fix map state mixin

--- src/main/java/net/modfest/fireblanket/mixin/opto/MixinMapState.java
+++ src/main/java/net/modfest/fireblanket/mixin/opto/MixinMapState.java
@@ -1,20 +1,18 @@
 package net.modfest.fireblanket.mixin.opto;
 
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.map.MapState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.function.Predicate;
 
 @Mixin(MapState.class)
 public class MixinMapState {
-	/**
-	 * @author jaskarth
-	 *
-	 * @reason don't
-	 */
-	@Overwrite
-	public void update(PlayerEntity player, ItemStack stack) {
-
+	@Redirect(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;contains(Ljava/util/function/Predicate;)Z"))
+	private boolean fireblanket$contains(PlayerInventory instance, Predicate<ItemStack> predicate) {
+		return false;
 	}
 }
