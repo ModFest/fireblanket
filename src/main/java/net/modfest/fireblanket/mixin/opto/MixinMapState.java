8337b5fd7f3a55c8928b3035bf04700f72f0f2ea
LunaFox <awakenedredstone@gmail.com>
2025-07-26 15:30:19 -0300

    fix: Use showDecorations as false to hide markers instead of inject (fixes maps failing to load)

--- src/main/java/net/modfest/fireblanket/mixin/opto/MixinMapState.java
+++ src/main/java/net/modfest/fireblanket/mixin/opto/MixinMapState.java
@@ -1,18 +1,19 @@
 package net.modfest.fireblanket.mixin.opto;
 
-import net.minecraft.entity.player.PlayerInventory;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.map.MapState;
+import org.spongepowered.asm.mixin.Debug;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Shadow;
 
-import java.util.function.Predicate;
-
+@Debug(export = true)
 @Mixin(MapState.class)
 public class MixinMapState {
-	@Redirect(method = "update", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerInventory;contains(Ljava/util/function/Predicate;)Z"))
-	private boolean fireblanket$contains(PlayerInventory instance, Predicate<ItemStack> predicate) {
-		return false;
+	@Shadow @Final private boolean showDecorations;
+
+	public MixinMapState() {}
+
+	{
+		this.showDecorations = false;
 	}
 }
