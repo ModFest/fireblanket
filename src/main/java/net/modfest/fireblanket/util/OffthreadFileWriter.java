8a5b649f202bf0da2767b5848f5c86ca014d2f1b
Ampflower 🌺 <git@ampflower.gay>
2025-08-23 08:01:20 -0700

    Add AsyncWriter
    This replaces the OffthreadFileWriter implementation with a writer that
    manages its own message queue and thread.
    
    Note: could use an audit and simplification by someone who knows the Java
    memory model regarding threading.

--- src/main/java/net/modfest/fireblanket/util/OffthreadFileWriter.java
+++ src/main/java/net/modfest/fireblanket/util/OffthreadFileWriter.java
@@ -1,56 +1,35 @@
 package net.modfest.fireblanket.util;
 
-import net.modfest.fireblanket.Fireblanket;
-
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
+import java.nio.file.Path;
 import java.util.Map;
-import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
-public class OffthreadFileWriter extends Thread {
-	private static final Thread THREAD = new OffthreadFileWriter();
-	private static final Map<File, String> FILE_MAP = new HashMap<>();
-	private static final Object WAIT_LOCK = new Object();
+public final class OffthreadFileWriter {
+	private static final Map<Path, AsyncWriter> FILE_MAP = new ConcurrentHashMap<>();
 
-	public OffthreadFileWriter() {
-	    super("Off-thread File Writer");
-		this.setDaemon(true);
+	@Deprecated
+	public static void write(String content, File file) {
+		write(file.toPath(), content);
 	}
 
-	@Override
-	public void run() {
+	public static void write(Path path, String content) {
 		try {
-			while (!this.isInterrupted()) {
-				synchronized (WAIT_LOCK) {
-					WAIT_LOCK.wait();
-				}
-
-				// Copy the set as it is a reference to a field in the map class, and changes to the map are reflected into it
-				Set<Map.Entry<File, String>> entries = Set.copyOf(FILE_MAP.entrySet());
-				FILE_MAP.clear();
-
-				for (Map.Entry<File, String> entry : entries) {
-					try (FileWriter writer = new FileWriter(entry.getKey(), true)) {
-						writer.append(entry.getValue());
-					} catch (IOException e) {
-						throw new RuntimeException(e);
-					}
+			// TODO: figure out a better locking strategy
+			AsyncWriter writer;
+			synchronized (FILE_MAP) {
+				writer = FILE_MAP.get(path);
+				if (writer == null) {
+					writer = AsyncWriter.bufferedWriter(path);
+					FILE_MAP.put(path, writer);
 				}
 			}
-		} catch (InterruptedException ignored) {}
-		Fireblanket.LOGGER.info("Fireblanket async file handler closed");
-	}
 
-	public static void write(String content, File file) {
-		FILE_MAP.merge(file, content, (s, s2) -> s + s2);
-		synchronized (WAIT_LOCK) {
-			WAIT_LOCK.notifyAll();
+			writer.write(content);
+		} catch (IOException io) {
+			// TODO: maybe swallow this depending on context
+			throw new RuntimeException("cannot write to " + path, io);
 		}
-	}
-
-	static {
-		THREAD.start();
 	}
 }
