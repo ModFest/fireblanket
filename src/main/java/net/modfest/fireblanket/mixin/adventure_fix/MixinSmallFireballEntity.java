f62d50de1e2979554ef4f01733b0766f24107427
Ampflower 🌺 <git@ampflower.gay>
2025-09-08 16:18:36 -0700

    Evidently, capturing locals don't work so well here.

--- src/main/java/net/modfest/fireblanket/mixin/adventure_fix/MixinSmallFireballEntity.java
+++ src/main/java/net/modfest/fireblanket/mixin/adventure_fix/MixinSmallFireballEntity.java
@@ -3,11 +3,14 @@ package net.modfest.fireblanket.mixin.adventure_fix;
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.projectile.AbstractFireballEntity;
 import net.minecraft.entity.projectile.SmallFireballEntity;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.GameRules;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
@@ -19,7 +22,11 @@ import org.spongepowered.asm.mixin.injection.At;
  * @author Ampflower
  **/
 @Mixin(SmallFireballEntity.class)
-public abstract class MixinSmallFireballEntity {
+public abstract class MixinSmallFireballEntity extends AbstractFireballEntity {
+
+	public MixinSmallFireballEntity(final EntityType<? extends AbstractFireballEntity> entityType, final World world) {
+		super(entityType, world);
+	}
 
 	@ModifyExpressionValue(
 		method = "onBlockHit",
@@ -28,15 +35,18 @@ public abstract class MixinSmallFireballEntity {
 			target = "Lnet/minecraft/world/World;isAir(Lnet/minecraft/util/math/BlockPos;)Z"
 		)
 	)
-	private static boolean fireblanket$madeAccurate(
+	private boolean fireblanket$madeAccurate(
 		final boolean original,
-		final @Local ServerWorld world,
-		final @Local Entity entity,
 		final @Local BlockPos blockPos
 	) {
 		if (!original) {
 			return false;
 		}
+
+		// we're already past the gate, don't bother to check.
+		final ServerWorld world = (ServerWorld) this.getWorld();
+		// Annoyingly, we can't just @Local it.
+		final Entity entity = this.getOwner();
 
 		if (entity == null && !world.getGameRules().getBoolean(GameRules.DO_FIRE_TICK)) {
 			return false;
