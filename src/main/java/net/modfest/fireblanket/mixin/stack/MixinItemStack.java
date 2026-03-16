452052f53e5aadf9e9444866a5b42a6a3b29acc8
Ampflower 🌺 <git@ampflower.gay>
2026-03-16 04:43:02 -0700

    Port IdStack'd ItemStack
    Detached from mixins.json, as it's unused.

--- src/main/java/net/modfest/fireblanket/mixin/stack/MixinItemStack.java
+++ src/main/java/net/modfest/fireblanket/mixin/stack/MixinItemStack.java
@@ -1,26 +1,31 @@
 package net.modfest.fireblanket.mixin.stack;
 
-import net.minecraft.component.ComponentMap;
+import net.minecraft.component.MergedComponentMap;
 import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
 import net.minecraft.registry.Registries;
 import net.modfest.fireblanket.mixinsupport.IdStack;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ItemStack.class)
-public class MixinItemStack { //implements IdStack {
-//	private int fireblanket$id = 0;
-//
-//	@Inject(method = "<init>(Lnet/minecraft/item/ItemConvertible;ILnet/minecraft/component/ComponentMapImpl;)V", at = @At("TAIL"))
-//	private void fireblanket$initFull(ItemConvertible item, int count, ComponentMapImpl components, CallbackInfo ci) {
-//		fireblanket$id = Registries.ITEM.getRawId(item.asItem());
-//	}
-//
-//	@Override
-//	public int fireblanket$getRawId() {
-//		return fireblanket$id;
-//	}
+public class MixinItemStack implements IdStack {
+	@Unique
+	private int fireblanket$id = 0;
+
+	@Inject(
+		method = "<init>(Lnet/minecraft/item/ItemConvertible;ILnet/minecraft/component/MergedComponentMap;)V",
+		at = @At("TAIL")
+	)
+	private void fireblanket$initFull(ItemConvertible item, int count, MergedComponentMap components, CallbackInfo ci) {
+		fireblanket$id = Registries.ITEM.getRawId(item.asItem());
+	}
+
+	@Override
+	public int fireblanket$getRawId() {
+		return fireblanket$id;
+	}
 }
