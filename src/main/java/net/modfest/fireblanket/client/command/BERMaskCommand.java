091c293ebf61240472cda1092f1ca498413257b2
Ampflower 🌺 <git@ampflower.gay>
2026-03-16 23:35:27 -0700

    Uncomment and fix BERMaskCommand

--- src/main/java/net/modfest/fireblanket/client/command/BERMaskCommand.java
+++ src/main/java/net/modfest/fireblanket/client/command/BERMaskCommand.java
@@ -23,82 +23,82 @@ import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.arg
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
 public class BERMaskCommand {
-//	public static void init(LiteralArgumentBuilder<FabricClientCommandSource> base, CommandRegistryAccess access) {
-//		base.then(literal("be")
-//			.then(literal("add")
-//				.then(argument("type", new BlockEntityTypeArgumentType(access))
-//					.executes(client -> {
-//						RegistryEntry.Reference<BlockEntityType<?>> type = getRegistryEntry(client, "type", RegistryKeys.BLOCK_ENTITY_TYPE);
-//
-//						if (type.getKey().isEmpty()) {
-//							client.getSource().sendFeedback(Text.literal("This BE seems to not be registered??"));
-//							return 1;
-//						}
-//
-//						client.getSource().sendFeedback(Text.literal("Added " + type.registryKey().getValue() + " to the mask."));
-//
-//						MinecraftClient.getInstance().submit(() -> ClientState.MASKED_BERS.add(type.value()));
-//						return 0;
-//					})
-//				)
-//			)
-//			.then(literal("remove")
-//				.then(argument("type", new BlockEntityTypeArgumentType(access))
-//					.executes(client -> {
-//						RegistryEntry.Reference<BlockEntityType<?>> type = getRegistryEntry(client, "type", RegistryKeys.BLOCK_ENTITY_TYPE);
-//
-//						if (type.getKey().isEmpty()) {
-//							client.getSource().sendFeedback(Text.literal("This BE seems to not be registered??"));
-//							return 1;
-//						}
-//
-//						client.getSource().sendFeedback(Text.literal("Removed " + type.registryKey().getValue() + " to the mask."));
-//
-//						MinecraftClient.getInstance().submit(() -> ClientState.MASKED_BERS.remove(type.value()));
-//						return 0;
-//					})
-//				)
-//			)
-//			.then(literal("list")
-//				.executes(client -> {
-//					if (ClientState.MASKED_BERS.isEmpty()) {
-//						client.getSource().sendFeedback(Text.literal("Your mask is empty."));
-//					} else {
-//						String string = ClientState.MASKED_BERS.stream().map(t -> Registries.BLOCK_ENTITY_TYPE.getId(t).toString()).collect(Collectors.joining(", "));
-//						client.getSource().sendFeedback(Text.literal("You are currently masking: [" + string + "]"));
-//					}
-//					return 0;
-//				})
-//			)
-//			.then(literal("clear")
-//				.executes(client -> {
-//					int size = ClientState.MASKED_BERS.size();
-//					MinecraftClient.getInstance().submit(ClientState.MASKED_BERS::clear);
-//
-//					client.getSource().sendFeedback(Text.literal("Cleared " + size + " BE" + (size == 1 ? "" : "s") + " out of the mask."));
-//					return 0;
-//				})
-//			)
-//		);
-//	}
-//
-//	private static final Dynamic3CommandExceptionType WRONG_TYPE_EXCEPTION = new Dynamic3CommandExceptionType(
-//		(tag, type, expectedType) -> Text.translatable("argument.resource_tag.invalid_type", tag, type, expectedType)
-//	);
-//
-//	public static <T> RegistryEntry.Reference<T> getRegistryEntry(CommandContext<FabricClientCommandSource> context, String name, RegistryKey<Registry<T>> registryRef) throws CommandSyntaxException {
-//		RegistryEntry.Reference<T> reference = context.getArgument(name, RegistryEntry.Reference.class);
-//		RegistryKey<?> registryKey = reference.registryKey();
-//		if (registryKey.isOf(registryRef)) {
-//			return reference;
-//		} else {
-//			throw WRONG_TYPE_EXCEPTION.create(registryKey.getValue(), registryKey.getRegistry(), registryRef.getValue());
-//		}
-//	}
-//
-//	public static class BlockEntityTypeArgumentType extends RegistryEntryArgumentType<BlockEntityType<?>> {
-//		protected BlockEntityTypeArgumentType(CommandRegistryAccess registryAccess) {
-//			super(registryAccess, RegistryKeys.BLOCK_ENTITY_TYPE, Registries.BLOCK_ENTITY_TYPE.getEntryCodec());
-//		}
-//	}
+	public static void init(LiteralArgumentBuilder<FabricClientCommandSource> base, CommandRegistryAccess access) {
+		base.then(literal("be")
+			.then(literal("add")
+				.then(argument("type", new BlockEntityTypeArgumentType(access))
+					.executes(client -> {
+						RegistryEntry.Reference<BlockEntityType<?>> type = getRegistryEntry(client, "type", RegistryKeys.BLOCK_ENTITY_TYPE);
+
+						if (type.getKey().isEmpty()) {
+							client.getSource().sendFeedback(Text.literal("This BE seems to not be registered??"));
+							return 1;
+						}
+
+						client.getSource().sendFeedback(Text.literal("Added " + type.registryKey().getValue() + " to the mask."));
+
+						MinecraftClient.getInstance().submit(() -> ClientState.MASKED_BERS.add(type.value()));
+						return 0;
+					})
+				)
+			)
+			.then(literal("remove")
+				.then(argument("type", new BlockEntityTypeArgumentType(access))
+					.executes(client -> {
+						RegistryEntry.Reference<BlockEntityType<?>> type = getRegistryEntry(client, "type", RegistryKeys.BLOCK_ENTITY_TYPE);
+
+						if (type.getKey().isEmpty()) {
+							client.getSource().sendFeedback(Text.literal("This BE seems to not be registered??"));
+							return 1;
+						}
+
+						client.getSource().sendFeedback(Text.literal("Removed " + type.registryKey().getValue() + " to the mask."));
+
+						MinecraftClient.getInstance().submit(() -> ClientState.MASKED_BERS.remove(type.value()));
+						return 0;
+					})
+				)
+			)
+			.then(literal("list")
+				.executes(client -> {
+					if (ClientState.MASKED_BERS.isEmpty()) {
+						client.getSource().sendFeedback(Text.literal("Your mask is empty."));
+					} else {
+						String string = ClientState.MASKED_BERS.stream().map(t -> Registries.BLOCK_ENTITY_TYPE.getId(t).toString()).collect(Collectors.joining(", "));
+						client.getSource().sendFeedback(Text.literal("You are currently masking: [" + string + "]"));
+					}
+					return 0;
+				})
+			)
+			.then(literal("clear")
+				.executes(client -> {
+					int size = ClientState.MASKED_BERS.size();
+					MinecraftClient.getInstance().submit(ClientState.MASKED_BERS::clear);
+
+					client.getSource().sendFeedback(Text.literal("Cleared " + size + " BE" + (size == 1 ? "" : "s") + " out of the mask."));
+					return 0;
+				})
+			)
+		);
+	}
+
+	private static final Dynamic3CommandExceptionType WRONG_TYPE_EXCEPTION = new Dynamic3CommandExceptionType(
+		(tag, type, expectedType) -> Text.translatable("argument.resource_tag.invalid_type", tag, type, expectedType)
+	);
+
+	public static <T> RegistryEntry.Reference<T> getRegistryEntry(CommandContext<FabricClientCommandSource> context, String name, RegistryKey<Registry<T>> registryRef) throws CommandSyntaxException {
+		RegistryEntry.Reference<T> reference = context.getArgument(name, RegistryEntry.Reference.class);
+		RegistryKey<?> registryKey = reference.registryKey();
+		if (registryKey.isOf(registryRef)) {
+			return reference;
+		} else {
+			throw WRONG_TYPE_EXCEPTION.create(registryKey.getValue(), registryKey.getRegistry(), registryRef.getValue());
+		}
+	}
+
+	public static class BlockEntityTypeArgumentType extends RegistryEntryArgumentType<BlockEntityType<?>> {
+		protected BlockEntityTypeArgumentType(CommandRegistryAccess registryAccess) {
+			super(registryAccess, RegistryKeys.BLOCK_ENTITY_TYPE, Registries.BLOCK_ENTITY_TYPE.getCodec());
+		}
+	}
 }
