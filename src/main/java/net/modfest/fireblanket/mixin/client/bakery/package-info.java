7f0fff6531f12708e97d3e9a2943e4996863d1c7
Ampflower 🌺 <git@ampflower.gay>
2026-05-19 00:47:25 -0700

    Disable the bakery in the event that Iris is present.
    Minimises surprise breakage without hard-breaks

--- src/main/java/net/modfest/fireblanket/mixin/client/bakery/package-info.java
+++ src/main/java/net/modfest/fireblanket/mixin/client/bakery/package-info.java
@@ -3,7 +3,9 @@
  */
 @NullMarked
 @Require(value = "glowcase", version = ">=2.5.0-", reason = "Bakery")
+@Conflict(value = "iris", reason = "Shaders don't cooperate with the bakery.")
 package net.modfest.fireblanket.mixin.client.bakery;
 
+import net.modfest.fireblanket.mixinsupport.modifiers.Conflict;
 import net.modfest.fireblanket.mixinsupport.modifiers.Require;
 import org.jspecify.annotations.NullMarked;
